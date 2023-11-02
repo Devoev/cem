e72d96665c9cb5ed0053d5b02232b60c2692d503
devin <devinbalian@gmail.com>
2023-11-02 13:10:03 +0100

    made plate curves transfinite with 50 elements

--- capacitor/generate_mesh.py
+++ capacitor/generate_mesh.py
@@ -2,29 +2,34 @@ import gmsh
 
 from util.mesh import model
 
-gm = gmsh.model.occ
+cad = gmsh.model.occ
+msh = gmsh.model.mesh
 
 
-@model("capacitor", dim=2, show_gui=True, finalize=True)
+@model("capacitor", dim=2, show_gui=True, finalize=True, options={"Mesh.MeshSizeFactor": 0.4})
 def generate_mesh(d: float, l: float, h: float, r: float):
     plate1 = add_plate(d, l, h)
     plate2 = add_plate(-d, l, -h)
-    air = gm.add_circle(0, 0, 0, r)
-    air = gm.add_curve_loop([air])
-    gm.add_plane_surface([air, plate1, plate2])
-    gm.synchronize()
+    air = cad.add_circle(0, 0, 0, r)
+    air = cad.add_curve_loop([air])
+    cad.add_plane_surface([air, plate1, plate2])
+    cad.synchronize()
 
 
 def add_plate(d: float, l: float, h: float) -> int:
     """Adds a plate of the capacitor to the geometry.
     :return: The tag of the curve loop.
     """
-    p1 = gm.add_point(l/2, (d + h)/2, 0)
-    p2 = gm.add_point(-l/2, (d + h)/2, 0)
-    p3 = gm.add_point(-l/2, (d - h)/2, 0)
-    p4 = gm.add_point(l/2, (d - h)/2, 0)
-    l1 = gm.add_line(p1, p2)
-    l2 = gm.add_line(p2, p3)
-    l3 = gm.add_line(p3, p4)
-    l4 = gm.add_line(p4, p1)
-    return gm.add_curve_loop([l1, l2, l3, l4])
+    p1 = cad.add_point(l / 2, (d + h) / 2, 0)
+    p2 = cad.add_point(-l / 2, (d + h) / 2, 0)
+    p3 = cad.add_point(-l / 2, (d - h) / 2, 0)
+    p4 = cad.add_point(l / 2, (d - h) / 2, 0)
+    l1 = cad.add_line(p1, p2)
+    l2 = cad.add_line(p2, p3)
+    l3 = cad.add_line(p3, p4)
+    l4 = cad.add_line(p4, p1)
+
+    cad.synchronize()
+    msh.set_transfinite_curve(l1, 30)
+    msh.set_transfinite_curve(l3, 30)
+    return cad.add_curve_loop([l1, l2, l3, l4])
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from capacitor.generate_mesh import generate_mesh
 
 if __name__ == '__main__':
-    generate_mesh(5, 8, 1, 15)
+    generate_mesh(8, 10, 0.5, 15)
 
