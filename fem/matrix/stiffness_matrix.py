5d93c94816ef106b5d70c37b2f38b631e570a15d
Devin <devinbalian@gmail.com>
2023-11-15 18:59:03 +0100

    stiffness_matrix.py updated comments updated implementation of area_triangle_2d in geo.py

--- fem/matrix/stiffness_matrix.py
+++ fem/matrix/stiffness_matrix.py
@@ -14,6 +14,6 @@ def stiffness_node_local(nodes: np.ndarray) -> np.ndarray:
     S = area_triangle_2d(nodes)
     J = np.vstack([a1 - a0, a2 - a0])                   # Jacobian
     G_inv = np.linalg.inv(J.T @ J)                      # Inverse gram matrix
-    grad_b = np.array([[-1, -1], [1, 0], [0, 1]]).T     # Gradients of nodal basis
+    grad_b = np.array([[-1, -1], [1, 0], [0, 1]]).T     # Gradients of nodal basis in reference triangle
 
     return S * grad_b.T @ G_inv @ grad_b
