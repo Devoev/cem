a16ff06bbb5215159ae98d818eddca04adb8bd2f
devin <devinbalian@gmail.com>
2023-11-15 16:32:21 +0100

    updated nodal mass matrix and added volume mass matrix

--- fem/matrix/mass_matrix.py
+++ fem/matrix/mass_matrix.py
@@ -6,9 +6,23 @@ from fem.mesh.geo import triangle_area
 def mass_node_local(nodes: np.ndarray) -> np.ndarray:
     """
     Creates the local mass matrix of nodal basis functions.
-    :param nodes: The triangle nodes. Matrix of size 2x3.
+    :param nodes: The triangle nodes in 2D. Matrix of size 2x3.
     :return: Local 3x3 mass matrix.
     """
 
     val = triangle_area(nodes) / 12
-    return np.full((3, 3), val) + np.diag([val, val, val])
+    return np.array([
+        [2*val, val, val],
+        [val, 2*val, val],
+        [val, val, 2*val]
+    ])
+
+
+def mass_vol_local(nodes: np.ndarray) -> float:
+    """
+    Creates the local mass matrix of volume basis functions.
+    :param nodes: Triangle nodes in 2D. Matrix of size 2x3.
+    :return: Local scalar mass matrix.
+    """
+
+    return 1/triangle_area(nodes)
