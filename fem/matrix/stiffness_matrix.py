d5b92568d4a5607187371e158c578ff109915f80
devin <devinbalian@gmail.com>
2023-11-15 17:32:26 +0100

    implemented stiffness_matrix.py for nodal basis

--- fem/matrix/stiffness_matrix.py
+++ fem/matrix/stiffness_matrix.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from util.geo import area_triangle_2d
+
 
 def stiffness_node_local(nodes: np.ndarray) -> np.ndarray:
     """
@@ -7,4 +9,11 @@ def stiffness_node_local(nodes: np.ndarray) -> np.ndarray:
     :param nodes: The triangle nodes in 2D. Matrix of size 2x3.
     :return: Local 3x3 mass matrix.
     """
-    pass
+
+    a0, a1, a2 = nodes.T
+    S = area_triangle_2d(nodes)
+    J = np.vstack([a1 - a0, a2 - a0])                   # Jacobian
+    G_inv = np.linalg.inv(J.T @ J)                      # Inverse gram matrix
+    grad_b = np.array([[-1, -1], [1, 0], [0, 1]]).T     # Gradients of nodal basis
+
+    return S * grad_b.T @ G_inv @ grad_b
--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import numpy as np
 from capacitor.generate_mesh import generate_mesh
 from fem.matrix import mass_matrix
 from fem.matrix.mass_matrix import mass_node_local
+from fem.matrix.stiffness_matrix import stiffness_node_local
 
 if __name__ == '__main__':
     # generate_mesh(8, 10, 0, 15)
 
-    nodes = np.array([[1, 0], [2, 2], [2, 6]]).T
-    mat = mass_node_local(nodes)
+    nodes = np.array([[0, 0], [2, 2], [2, 6]]).T
+    M = mass_node_local(nodes)
+    K = stiffness_node_local(nodes)
