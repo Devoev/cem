9713dca64a29b9084fbd35cc15ab6c415dedadd5
Devin <devinbalian@gmail.com>
2023-12-12 17:42:27 +0100

    fix build_nodal_mat.py

--- fem/matrix/build_nodal_mat.py
+++ fem/matrix/build_nodal_mat.py
@@ -14,13 +14,13 @@ def build_nodal_mat(msh: Mesh2D, build_local_mat: Callable[[int], np.ndarray]) -
     :return: Global matrix.
     """
 
-    n = 9 * msh.E                       # Amount of matrix entries
+    n = 9 * msh.T                       # Amount of matrix entries
     N = msh.N                           # Matrix dimension
     vals = np.zeros(n)                  # Nonzero values of tge matrix
     rows = np.zeros(n, dtype='int')     # Row indices for the entries
     cols = np.zeros(n, dtype='int')     # Column indices for the entries
 
-    for e in range(msh.E):
+    for e in range(msh.T):
         nodes = msh.elems_to_nodes[e]
         idx = np.arange(9*e,9*(e+1))
 
--- main.py
+++ main.py
@@ -10,12 +10,12 @@ if __name__ == '__main__':
     generate_mesh(8, 10, 0, 15)
     msh = make_mesh()
 
-    # M = mass_node(msh).toarray()
-    # K = stiffness_node(msh).toarray()
-    # plt.spy(M, markersize=0.1)
-    # plt.figure()
-    # plt.spy(K, markersize=0.1)
-    # plt.show()
-    #
-    # print(M.shape)
-    # print(np.linalg.matrix_rank(M))
+    M = mass_node(msh).toarray()
+    K = stiffness_node(msh).toarray()
+    plt.spy(M, markersize=0.1)
+    plt.figure()
+    plt.spy(K, markersize=0.1)
+    plt.show()
+
+    print(M.shape)
+    print(np.linalg.matrix_rank(M))
