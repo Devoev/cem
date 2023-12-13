88899ac04d03bb80dee08ec8ddb7eb24d10ebdb7
Devin <devinbalian@gmail.com>
2023-12-13 16:36:16 +0100

    add basis_node.py

---
+++ fem/basis/basis_node.py
@@ -0,0 +1,16 @@
+from typing import Callable, Tuple
+
+import numpy as np
+
+ndfun = Callable[[np.ndarray], float]
+"""Function from R^n -> R."""
+
+
+def basis_node_ref() -> Tuple[ndfun, ndfun, ndfun]:
+    """Returns the three nodal basis functions on the reference triangle."""
+    return lambda p: 1 - p[0] - p[1], lambda p: p[0], lambda p: p[1]
+
+
+def basis_node_ref_grad() -> np.ndarray:
+    """Returns the constant gradients of nodal basis functions on the reference triangle. Matrix of size ``(2,3)``."""
+    return np.array([[-1, -1], [1, 0], [0, 1]]).T
