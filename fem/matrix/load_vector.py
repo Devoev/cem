97296f40812d45c6336c8edf9b62c7b8b94d2e11
devin <devinbalian@gmail.com>
2023-12-13 13:16:06 +0100

    add mass_load_local in load_vector.py

---
+++ fem/matrix/load_vector.py
@@ -0,0 +1,23 @@
+from typing import Callable, List
+
+import numpy as np
+
+from util.geo import area_triangle_2d
+from util.quadrature import int_ref_triangle_2d
+
+
+def mass_load_local(f: Callable[[np.ndarray], float], nodes: np.ndarray, q: int):
+    """
+    Creates the local load vector of nodal basis functions.
+    :param f: Right hand side function.
+    :param nodes: The triangle nodes in 2D. Matrix of size ``(2,3)``.
+    :param q: Order of integration.
+    :return: Local load vector of size ``(3)``.
+    """
+
+    a0, a1, a2 = nodes
+    J = np.vstack([a1 - a0, a2 - a0]).T
+    S = area_triangle_2d(nodes.T)
+    b: List[Callable[[np.ndarray], float]] = [lambda p: 1 - p[0] - p[1], lambda p: p[0], lambda p: p[1]]
+    return 2*S*np.array([int_ref_triangle_2d(lambda p: f(a0 + J@p) * b[i](p), q) for i in range(3)])
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from capacitor.generate_mesh import generate_mesh
+from fem.matrix.load_vector import mass_load_local
 from fem.matrix.mass_matrix import mass_node, mass_edge_local, mass_node_local, mass_vol_local
 from fem.matrix.stiffness_matrix import stiffness_node
 from fem.mesh.mesh_2d import make_mesh
@@ -10,11 +11,10 @@ if __name__ == '__main__':
     generate_mesh(8, 10, 0, 15)
     msh = make_mesh()
 
-    # nodes = np.array([[0,1,0], [0,0,1]])
-    nodes = np.array([[0,0], [2,0], [2,6]]).T
-    mass_s1 = mass_node_local(nodes)
-    mass_r1 = mass_edge_local(nodes.T)
-    mass_q0 = mass_vol_local(nodes)
+    nodes = np.array([[0,1,0], [0,0,1]]).T
+    # nodes = np.array([[0,0], [2,0], [2,6]]).T
+
+    load_vec = mass_load_local(lambda p: 1, nodes, 1)
 
     # M = mass_node(msh).toarray()
     # K = stiffness_node(msh).toarray()
