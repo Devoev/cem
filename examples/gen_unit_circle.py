abb87240786e9dc94473e7a6bf4dd6161b89cbfe
devin <devinbalian@gmail.com>
2023-12-26 22:15:54 +0100

    update gen_unit_circle.py to embed coordinate origin into mesh update point_charge.ipynb

--- examples/gen_unit_circle.py
+++ examples/gen_unit_circle.py
@@ -3,11 +3,16 @@ import gmsh
 from util.gmsh_model import gmsh_model
 
 cad = gmsh.model.occ
+msh = gmsh.model.mesh
 
 
-@gmsh_model("unit_circle", dim=2, finalize=False, options={"Mesh.MeshSizeFactor": 0.2})
+@gmsh_model("unit_circle", dim=2, finalize=False, options={"Mesh.MeshSizeFactor": 0.3, "Mesh.MeshSizeMax": 0.3})
 def gen_unit_circle():
+    p = cad.add_point(0,0,0)
     c = cad.add_circle(0,0,0,1)
     boundary = cad.add_curve_loop([c])
-    cad.add_plane_surface([boundary])
+    omega = cad.add_plane_surface([boundary])
+
+    cad.synchronize()
+    msh.embed(0, [p], 2, omega)
     cad.synchronize()
